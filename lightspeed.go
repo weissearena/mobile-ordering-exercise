cb5871f8a0178f38e5bd0779f6027b3550a6c329
Leo Baker-Hytch <leo.bakerhytch@laax.com>
2023-04-22 14:56:31 +0200

    Add lightspeed

---
+++ lightspeed.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Order struct {
+	ID          int         `json:"id"`
+	TableNumber int         `json:"tableNumber"`
+	Timestamp   int         `json:"timestamp"`
+	Items       []OrderItem `json:"items"`
+}
+
+type OrderItem struct {
+	ID       string `json:"id"`
+	Price    int    `json:"price"`
+	Quantity int    `json:"quantity"`
+}
+
+func main() {
+	http.HandleFunc("/", handleOrder)
+	log.Fatal(http.ListenAndServe(":4000", nil))
+}
+
+func handleOrder(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var order Order
+	err := json.NewDecoder(r.Body).Decode(&order)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Calculate total price in CHF
+	totalPrice := float64(0)
+	for _, item := range order.Items {
+		totalPrice += float64(item.Price * item.Quantity)
+	}
+
+	// Convert total price to CHF
+	totalPrice /= 100 // convert rappen to CHF
+
+	// Print out order summary
+	fmt.Printf("Order #%d @ %d\t\tTable %d\t\tCHF %.2f\n", order.ID, order.Timestamp, order.TableNumber, totalPrice)
+}
