f10c8b5309a7d5017379315eb83ce5a65c69516a
Leo Baker-Hytch <leo.bakerhytch@laax.com>
2023-05-11 23:26:51 +0200

    Reformat code

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from 'express'
+import bodyParser from 'body-parser'
 
-const app = express();
-app.use(bodyParser.json());
+const app = express()
+app.use(bodyParser.json())
 
-const startTimestamp = Date.now();
-let id = 0;
+const startTimestamp = Date.now()
+let id = 0
 
 app.post('/orders', (req, res) => {
     const order = {
         id: id++,
         timestamp: Date.now() - startTimestamp,
         ...req.body,
-    };
+    }
 
-    console.log(order);
-    res.status(201).send({ status: 'OK' });
-});
+    console.log(order)
+    res.status(201).send({ status: 'OK' })
+})
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+    console.log('Server is running on port 3000')
+})
