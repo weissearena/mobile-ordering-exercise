04ba7425ebf686e1593e8b32fa3c7621b64d4c57
Leo Baker-Hytch <leo.bakerhytch@laax.com>
2023-05-11 23:26:14 +0200

    Move server to index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-function main() {}
+import express from 'express';
+import bodyParser from 'body-parser';
 
-main();
+const app = express();
+app.use(bodyParser.json());
+
+const startTimestamp = Date.now();
+let id = 0;
+
+app.post('/orders', (req, res) => {
+    const order = {
+        id: id++,
+        timestamp: Date.now() - startTimestamp,
+        ...req.body,
+    };
+
+    console.log(order);
+    res.status(201).send({ status: 'OK' });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
